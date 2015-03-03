6c282933601d07d78e19d0660954cd732d74db06
blakely <blakely@google.com>
2015-03-03 12:29:16 -0800

    Sup World? :D USART works in transmit mode.

--- boot_main.c
+++ boot_main.c
@@ -13,10 +13,6 @@
 #define	BOOTLOADER_BLINK_LOOP_COUNT	(F_CPU / 2250)
 #define MAIN_BLINK_LOOP_COUNT BOOTLOADER_BLINK_LOOP_COUNT * 4
 
-#ifndef BAUDRATE
-	#define BAUDRATE 115200
-#endif
-
 /*
  * Calculate the address where the bootloader starts from FLASHEND and BOOTSIZE
  * (adjust BOOTSIZE below and BOOTLOADER_ADDRESS in Makefile if you want to change the size of the bootloader)
@@ -42,6 +38,12 @@
 #define APP_END  (FLASHEND -(2*BOOTSIZE) + 1)
 
 void (*application_entry_addr)(void) = 0x0000;
+
+#define F_CPU 16000000UL
+#define BAUD 115200
+#define BAUD_TOL 3
+#include <util/setbaud.h>
+
 
 /* 
   Entry point into the bootloader. 
@@ -111,11 +113,52 @@ ISR(INT1_vect)
   }
 }
 
-void SetMCUR() {
+void SetMCUR(void) {
   // Set IVCE to one so we can change IVSEL
   MCUCR =  0x1;
   // Set the IVSEL bit to make sure the interrupts in the bootloader are used
   MCUCR = 0x2;
+}
+
+void InitUart(void) {
+  // Set the baud rate
+  UBRR0H = UBRRH_VALUE;
+  UBRR0L = UBRRL_VALUE;
+
+  // Status status register is UCSR0A
+  // Control register is UCSR0B
+
+  // Set 2X mode
+  UCSR0A |= 1 << 1;
+ 
+  // Need to enable transmission on control reg B, which I believe is bit 3?
+  UCSR0B |= 1 << 3;
+
+  // Set char size to 8
+  UCSR0B &= ~(1 << UCSZ02); // bit 2
+  UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); // Bits 2, 1
+ 
+  // Set to asynchronous mode
+  UCSR0C &= ~(1 << UMSEL01 | 1 << UMSEL00); // Bits 7, 6
+
+  // No parity
+  UCSR0C &= ~(1 << UPM01 | 1 << UPM00);  // Bits 5, 4
+  
+  // One stop
+  UCSR0C |= 1 << USBS0; // Bit 3
+
+  // Async mode requires Clock Polarity to be rising transmit
+  UCSR0C &= ~(1 << UCPOL0); // Bit 0
+}
+
+void SerialWrite(char c) {
+  // set the actual 8 bits to whatever the char is
+  UDR0 = c;
+  // Now that we've set the usart data register, let's wait until it's transmitted. Technically, we could do other things in the meantime I guess...
+  // We need to check the status register USCR0A to make sure that the transmitssion completes. Sets it to 1 when it is complete. The actual bit is TXC0 on the arduino mega 2560, which translates to bit6 of UCSR0A
+  while (!(UCSR0A & (1 << 6))) {}
+  // The byte has been sent off! Now we need to re-set that bit so we know what to look for next time.
+  UCSR0A |= 1 << 6;
 }
 
 int main() {
@@ -129,6 +172,13 @@ int main() {
   INPORT = 0b00000000;
 
   SetMCUR();
+
+  InitUart();
+
+  SerialWrite('S');
+  SerialWrite('u');
+  SerialWrite('p');
+  SerialWrite('?');
 
   //set PD0/INT0 as input
   DDRD |= 0 << PD0;
